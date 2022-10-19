a267ff538ee52a09f0c9fdb50d8edf566792a758
Andele97 <andreapinna40@live.it>
2022-10-19 19:52:11 +0200

    [finish] script

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -5,8 +5,40 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
-
 const el = document.querySelector.bind(document);
 
-const choiseUser =  el('.sceltaUente');
-const 
+const choiseUser =  el('.sceltaUtente');
+const numbUser = el('input');
+const btn = el('button');
+const winner = el('.output');
+const outputSomma = el('.outputSomma');
+
+btn.addEventListener('click', function(){
+
+  if(numbUser.value < 1 || numbUser.value > 5){
+    winner.innerText = `Il numero inserito non è valido inserisci un numero da 1 a 5`
+    return
+  }else{
+    let pcNumb = randomNumb();
+    if(choiseUser.value ===  pariDispari(parseInt(numbUser.value), pcNumb)){
+      winner.innerText = 'User vince'
+    }else{
+      winner.innerText = 'Pc vince'
+    }
+  }
+})
+
+//FUNZIONE
+
+function randomNumb(){
+  return Math.floor(Math.random() * 5);
+}
+
+function pariDispari(numbUs, numbPc){
+
+  let somma = numbUs + numbPc;
+  outputSomma.innerText = `la somma è: ${somma}`
+  if(somma % 2 == 0) return 'pari';
+
+  return 'dispari';
+}
