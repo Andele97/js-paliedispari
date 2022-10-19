122dcd3845759cb9e95addf6415c5d11259e4ebe
Andele97 <andreapinna40@live.it>
2022-10-19 18:24:45 +0200

    [add] structure html

---
+++ Pari e dispari/js/script.js
@@ -0,0 +1,12 @@
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+const el = document.querySelector.bind(document);
+
+const choiseUser =  el('.sceltaUente');
+const 
